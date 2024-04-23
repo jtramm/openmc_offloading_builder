4de4a1a5ae783ff7aadd987f565124adf03ba6ad
John Tramm <john.tramm@gmail.com>
2024-04-23 09:30:33 -0700

    Modernized LLVM build script

--- build_llvm.sh
+++ build_llvm.sh
@@ -14,15 +14,19 @@ set -xe
 # on my cluster.
 # If you want to use gcc or something else, you'll need to also edit the cmake line to point
 # to your descired c and c++ compilers
-module load llvm/release-11.0.1
+#module load llvm/release-11.0.1
 
 # We also need CMake
-module load spack
 module load cmake
+module load spack
+#module load ninja
+module load rocm/6.0.0
 
 # These are the options we will set for this build
-PACKAGES="clang;compiler-rt;lld"
-RUNTIMES="libcxxabi;libcxx;openmp"
+PROJECTS="clang;lld"
+RUNTIMES="openmp"
+TARGETS="all"
+USE_CCACHE=OFF
 
 # This script will make two folders in the current directly: llvm-build, llvm-install
 rm -rf llvm-build
@@ -33,48 +37,25 @@ mkdir llvm-install
 
 cd llvm-build
 
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../llvm-install \
-    -DLLVM_ENABLE_BACKTRACES=ON \
-    -DLLVM_ENABLE_WERROR=OFF \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DLLVM_ENABLE_RTTI=OFF \
+#cmake -G "Ninja" \
+cmake \
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DLLVM_ENABLE_PROJECTS=${PROJECTS}  \
+    -DLLVM_ENABLE_RUNTIMES=${RUNTIMES}   \
+    -DLLVM_TARGETS_TO_BUILD=${TARGETS}   \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_ENABLE_PROJECTS="$PACKAGES" \
-    -DLLVM_ENABLE_RUNTIMES="$RUNTIMES" \
-    -DOPENMP_ENABLE_LIBOMPTARGET=ON \
-    -DLIBOMPTARGET_NVPTX_COMPUTE_CAPABILITIES=35,60,70,75,80 \
-    -DCLANG_OPENMP_NVPTX_DEFAULT_ARCH=sm_80 \
-    -DLIBOMPTARGET_NVPTX_ENABLE_BCLIB=ON \
     -DLIBOMPTARGET_ENABLE_DEBUG=ON \
+    -DLLVM_OPTIMIZED_TABLEGEN=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DLLVM_CCACHE_BUILD=${USE_CCACHE} \
+    -DLLVM_APPEND_VC_REV=OFF \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_CXX_FLAGS="-Wno-address" \
+    -DCMAKE_INSTALL_PREFIX=../llvm-install \
     ../llvm-project/llvm
 
+#ninja -j32
+#ninja install
 make -j32 install
-
-# Once installed, you'll need to update your environment as:
-
-# export PATH=/path/to/llvm-install/bin:$PATH
-# export LD_LIBRARY_PATH=/path/to/llvm-install/lib:$LD_LIBRARY_PATH
-# export LIBRARY_PATH=/path/to/llvm-install/lib:$LIBRARY_PATH
-
-# When running the compiler on NVIDIA, you'll also need to load CUDA:
-
-# module load cuda
-
-
-# Example of a module file you might create to make the installation easier:
-
-#%Module
-
-# proc ModulesHelp { } {
-#    puts stderr "This module adds LLVM with OpenMP Offloading to your path"
-# }
-# 
-# module-whatis "This module adds LLVM with OpenMP Offloading to your path\n"
-# 
-# set basedir "/path/to/llvm-install"
-# prepend-path PATH "${basedir}/bin"
-# prepend-path LD_LIBRARY_PATH "${basedir}/lib"
-# prepend-path LIBRARY_PATH "${basedir}/lib"
-# module load cuda
